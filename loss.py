b79d9dc740b8a1b5c426f2ce2d51efdf059a1407
quiltacv <quilt@atmarkcafe.org>
2018-10-06 11:01:22 +0700

    rename value

--- loss.py
+++ loss.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy
-a = [0,1,2,3,4]
+omega = [0,1,2,3,4]
 data = [[1,2],[2,4],[3,6]]
 lost = []
 def forward(x,y,w):
     return (w*x-y)**2
-for i in a:
-    sum = 0
+for w in omega:
+    sum_loss_w = 0
     for item in data:
-        sum += forward(item[0], item[1], i)
-    lost.append(round(sum / len(data),1))
+        sum_loss_w += forward(item[0], item[1], w)
+    lost.append(round(sum_loss_w / len(data),1))
 print(lost)
 plt.plot(lost)
 plt.ylabel("fox x")
