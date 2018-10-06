6dd2141a04ab132739471116bd26294b81628f37
quiltacv <quilt@atmarkcafe.org>
2018-10-06 11:14:17 +0700

    change omage

--- loss.py
+++ loss.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
-import numpy
-omega = [0,1,2,3,4]
 data = [[1,2],[2,4],[3,6]]
 lost = []
+
 def forward(x,y,w):
     return (w*x-y)**2
-for w in omega:
+w = 0
+
+while w < 4:
     sum_loss_w = 0
     for item in data:
         sum_loss_w += forward(item[0], item[1], w)
-    lost.append(round(sum_loss_w / len(data),1))
+    lost.append(sum_loss_w / len(data))
+    w += 0.1
 print(lost)
 plt.plot(lost)
 plt.ylabel("fox x")
